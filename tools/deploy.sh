3b4246bcf6d22ebacfc7c487b9e33da14ff6d555
Thomas Wu <ixnij.wu@outlook.com>
2021-07-06 21:59:16 +0800

    modified:   deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh 
 
-if [ -e "~/.zshrc" ] || [ -e "~/.zshrc.local" ]; then 
-	echo "There's no any zshrc."
+CONFIGROOT=".."
+
+if [ -e "~/.zshrc" ] && [ -e "~/.zshrc.local" ]; then 
+	echo "There's already has zsh configuration files." >> /dev/stderr
+elif [ -e "~/.zshrc.local" ]; then 
+	curl -Lo .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc	
 else
-	echo "There's already has zsh configuration files."
+	curl -Lo .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+	ln -s ${CONFIGROOT}/zsh/zshrc.local ~/.zshrc.local  
 fi
