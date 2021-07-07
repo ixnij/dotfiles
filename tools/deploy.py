3cf217c7041d7efcfd0158d6ed913cc6fd8b86d3
Thomas Wu <ixnij.wu@outlook.com>
2021-07-07 12:05:28 +0800

    2021年 7月 7日 星期三 12时05分27秒 CST

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from os import *
-def ls(dir = "/"):
-    for d in listdir(dir):
-        if path.isdir(d):
-            print(d)
-ls()
+import os
+
+def fd(dry = "/"):
+    for i in os.listdir(dry):
+        if os.path.isdir(i):
+            print(i)
+fd()
