6904cd9a3dd340a565c08a67ba431db1689d0fe6
Thomas Wu <ixnij.wu@outlook.com>
2021-07-07 12:00:13 +0800

    2021年 7月 7日 星期三 12时00分12秒 CST

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"Python script that help me deploy my configuration files."
-
-__author__ = "Thomas Wu <ixnij.wu@outlook.com>"
-
-class Person(object):
-    
-    def __init__(self, name):
-        self.name = name
-
+from os import *
+def ls(dir = "/"):
+    for d in listdir(dir):
+        if path.isdir(d):
+            print(d)
+ls()
