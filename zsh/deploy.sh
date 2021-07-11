d18b68ca444d9c7b3029c63964aa0c987fec58aa
Thomas Wu <ixnij.wu@outlook.com>
2021-07-11 12:39:11 +0800

    update.

--- zsh/deploy.sh
+++ zsh/deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
+### Filename: deploy.sh 
+### Author: Thomas Wu <ixnij.wu@outlook.com>
+### TODO: sh -> py
 
 ZSHRC="$HOME/.zshrc"
 if [[ ! -f "$ZSHRC" ]]; then
-	wget 
+	wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+#else
+#	mv $ZSHRC $ZSHRC.bak
+#	wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+fi
