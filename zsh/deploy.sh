0173b80e791f877e87947e39c832d4e94d9638b1
Thomas Wu <ixnij.wu@outlook.com>
2021-07-11 12:40:12 +0800

    update.

--- zsh/deploy.sh
+++ zsh/deploy.sh
@@ -4,9 +4,14 @@
 ### TODO: sh -> py
 
 ZSHRC="$HOME/.zshrc"
+ZSHRC_LOCAL="$HOME/.zshrc.local"
+
 if [[ ! -f "$ZSHRC" ]]; then
 	wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 #else
 #	mv $ZSHRC $ZSHRC.bak
 #	wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 fi
+
+if [[ ! -f "ZSHRC_LOCAL" ]]; then
+	ln -s 
