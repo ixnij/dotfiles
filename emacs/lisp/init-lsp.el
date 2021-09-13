ce50a9790e237253e1cce47e4953b46c42b967e6
Thomas Wu <ixnij.wu@gmail.com>
2021-09-13 22:10:16 +0800

    Update.

--- emacs/lisp/init-lsp.el
+++ emacs/lisp/init-lsp.el
@@ -6,8 +6,10 @@
 
 (when (maybe-require-package 'eglot)
   (require 'eglot)
-  (add-to-list 'eglot-server-programs '(c-or-c++-mode  . ("/Library/Developer/CommandLineTools/usr/bin/clangd" "--background-index")))
-  (add-hook 'c-or-c++-mode-hook 'eglot-ensure))
+  (add-to-list 'eglot-server-programs '(c++-mode  . ("/Library/Developer/CommandLineTools/usr/bin/clangd" "--background-index")))
+  (add-to-list 'eglot-server-programs '(c-mode  . ("/Library/Developer/CommandLineTools/usr/bin/clangd" "--background-index")))
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+  (add-hook 'c-mode-hook 'eglot-ensure))
 
 (provide 'init-lsp)
 ;;; Local Variables:
