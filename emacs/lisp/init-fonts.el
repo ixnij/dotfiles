761a16db6434c7420e38aa01c7cb931d1d00de3e
Thomas Wu <ixnij.wu@gmail.com>
2021-09-13 11:49:12 +0800

    Update, For font-settings.
    	Some lisp code copied from centaur emacs.

--- emacs/lisp/init-fonts.el
+++ emacs/lisp/init-fonts.el
@@ -1,4 +1,39 @@
-; (set-face-attribute 'default nil :font "Iosevka" :height 160)
-(set-face-attribute 'default nil :font "SF Mono" :height 140)
+;;; init-fonts.el --- Settings for fonts.  -*- lexical-binding: t; -*-
+
+;;; Commentary
+
+;; (set-face-attribute 'default nil :font "Iosevka" :height 160)
+;; (set-face-attribute 'default nil :font "SF Mono" :height 140)
+
+;;; Code:
+
+(require 'cl-lib)
+
+(defun font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
+
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("SF Mono" "Iosevka" "Source Code Pro" "Fira Code"
+			 "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+	   when (font-installed-p font)
+	   return (set-face-attribute 'default nil
+				      :font font
+				      :height 160))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+	   when (font-installed-p font)
+	   return(set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+	   when (font-installed-p font)
+	   return (set-fontset-font t '(#x4e00 . #x9fff) font)))
 
 (provide 'init-fonts)
+;;; Local Variables:
+;;; coding: utf-8
+;;; End:
+;;; init-fonts.el ends here
