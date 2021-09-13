97cb9805449173de1392b0e7c4106113813d187e
Thomas Wu <ixnij.wu@gmail.com>
2021-09-13 12:03:16 +0800

    Update, For font-settings

--- emacs/lisp/init-fonts.el
+++ emacs/lisp/init-fonts.el
@@ -13,24 +13,29 @@
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
 
+(defvar ixnij/default-fonts '("Jetbrains Mono"
+			      "Fira Code Retina"
+			      "SF Mono"
+			      "Iosevka")
+  "This is My favorite fonts list.")
+
 (when (display-graphic-p)
   ;; Set default font
-  (cl-loop for font in '("SF Mono" "Iosevka" "Source Code Pro" "Fira Code"
-			 "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
-	   when (font-installed-p font)
-	   return (set-face-attribute 'default nil
+  (cl-loop for font in ixnij/default-fonts
+   when (font-installed-p font)
+   return (set-face-attribute 'default nil
 				      :font font
 				      :height 160))
 
   ;; Specify font for all unicode characters
   (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
-	   when (font-installed-p font)
-	   return(set-fontset-font t 'unicode font nil 'prepend))
+   when (font-installed-p font)
+   return(set-fontset-font t 'unicode font nil 'prepend))
 
   ;; Specify font for Chinese characters
   (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-	   when (font-installed-p font)
-	   return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+   when (font-installed-p font)
+   return (set-fontset-font t '(#x4e00 . #x9fff) font)))
 
 (provide 'init-fonts)
 ;;; Local Variables:
