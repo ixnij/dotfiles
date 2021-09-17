62f1361a080335dcf84055e7334a230882e29081
Thomas Wu <ixnij.wu@gmail.com>
2021-09-17 21:09:25 +0800

    Update: Dropped eglot, use lsp-mode instead.

--- emacs/lisp/init-lsp.el
+++ emacs/lisp/init-lsp.el
@@ -4,13 +4,6 @@
 
 ;;; Code:
 
-(when (maybe-require-package 'eglot)
-  (require 'eglot)
-  (add-to-list 'eglot-server-programs '(c++-mode  . ("/Library/Developer/CommandLineTools/usr/bin/clangd" "--background-index")))
-  (add-to-list 'eglot-server-programs '(c-mode  . ("/Library/Developer/CommandLineTools/usr/bin/clangd" "--background-index")))
-  (add-hook 'c++-mode-hook 'eglot-ensure)
-  (add-hook 'c-mode-hook 'eglot-ensure))
-
 (provide 'init-lsp)
 ;;; Local Variables:
 ;;; coding: utf-8
