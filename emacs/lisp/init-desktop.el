62b587e6834ffb1891e2b230ea731096998539da
Thomas Wu <ixnij.wu@gmail.com>
2021-09-21 11:54:27 +0800

    Update[emacs]: for desktop.

--- emacs/lisp/init-desktop.el
+++ emacs/lisp/init-desktop.el
@@ -3,8 +3,17 @@
 ;;; Commentary:
 
 ;;; Code:
+
+(require 'desktop)
 (setq desktop-path (list user-emacs-directory)
-      desktop-auto-save-timeout 600)
-(desktop-save-mode 1)
+      desktop-auto-save-timeout 600
+      desktop-restore-frames nil
+      desktop-file-modtime (file-attribute-modification-time
+			    (file-attributes (desktop-full-file-name)))
+      desktop-save t
+      desktop-lazy-verbose nil
+      desktop-restore-eager t)
+(desktop-save-mode t)
+
 (provide 'init-desktop)
 ;;; init-desktop.el ends here
