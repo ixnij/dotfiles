4baa034c71b0f13390d821583cccc61b642ca790
Thomas Wu <ixnij.wu@gmail.com>
2021-10-01 15:37:14 +0800

    [emacs]: add a rule that disable the awesome-tab in tty.

--- emacs/lisp/init-site-lisp.el
+++ emacs/lisp/init-site-lisp.el
@@ -7,7 +7,8 @@
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 
 (require 'awesome-tab)
-(awesome-tab-mode t)
+(when (display-graphic-p)
+  (awesome-tab-mode t))
 
 (provide 'init-site-lisp)
 ;;; init-site-lisp.el ends here
