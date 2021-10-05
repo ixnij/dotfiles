016bffcd77783e8d0cf52ad12bb2e46ef1916c0a
Thomas Wu <ixnij.wu@gmail.com>
2021-10-05 21:12:05 +0800

    [nix]: update, add home dot nix.

---
+++ nix/home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, callPackage, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "wu";
+  home.homeDirectory = "/Users/wu";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  home.packages = [
+    pkgs.htop
+    pkgs.fortune
+    pkgs.zoxide
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.fzf
+    pkgs.bat
+    pkgs.emacs
+  ];
+
+}
