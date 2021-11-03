2f8bbc368fca6ce9e1693503753d77e01f8770a8
Thomas Wu <ixnij.wu@gmail.com>
2021-11-03 21:30:49 +0800

    [emacs] remove some useless variables, and indent..

--- emacs/init.el
+++ emacs/init.el
@@ -5,77 +5,65 @@
 ;; This file bootstraps full configuretion.
 
 ;;; Code:
-
-(let ((minver "25.1"))
-  (when (version< emacs-version minver)
+(let
+    ((minver "25.1"))
+  (when
+      (version< emacs-version minver)
     (error "This Emacs is too old to use.-- You need to update to %s or higher" minver)))
-(when (version< emacs-version "27.1")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
-
+(when
+(version< emacs-version "27.1")
+(message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
-
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
+(add-to-list 'load-path
+(expand-file-name "lisp" user-emacs-directory))
 
-
-(defconst system-darwin-p (eq system-type 'darwin))
-
+(defconst system-darwin-p
+(eq system-type 'darwin))
+(defconst system-linux-p
+(eq system-type 'gnu/linux))
 
-
-(when (display-graphic-p)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
-
+(when
+(display-graphic-p)
+(tool-bar-mode -1)
+(scroll-bar-mode -1))
 
-
-(defvar ixnij/enable-pyim-p t
-  "Is pyim should be enable?")
-(defvar ixnij/enable-emacs-rime-p nil
-  "Is emacs-rime should be enable?")
-(unless ixnij/enable-pyim-p
-  (setq ixnij/enable-emacs-rime-p t))
-(when ixnij/enable-pyim-p
-  (defvar ixnij/pyim-use-rime-p nil
-    "Should pyim use rime?"))
-(defvar ixnij/display-line-number-p nil)
-(defvar ixnij/vertico-use-posframe-p t)
+(defvar ixnij/vertico-use-posframe-p nil)
 
-
 (require 'init-native-comp)
 (require 'init-mackeys)
 ;; (require 'init-fonts)
-
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'emacs-startup-hook
-	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-
+(let
+((normal-gc-cons-threshold
+(* 20 1024 1024))
+(init-gc-cons-threshold
+(* 128 1024 1024)))
+(setq gc-cons-threshold init-gc-cons-threshold)
+(add-hook 'emacs-startup-hook
+(lambda
+()
+(setq gc-cons-threshold normal-gc-cons-threshold))))
+(setq custom-file
+(expand-file-name "custom.el" user-emacs-directory))
 (require 'init-site-lisp)
 (require 'init-elpa)
 (package-initialize)
 (require-package 'diminish)
 (require-package 'posframe)
-
 (require 'init-misc)
 (require 'init-evil)
-
-(when (maybe-require-package 'ace-window)
-  (global-set-key (kbd "M-o") #'ace-window))
-
+(when
+(maybe-require-package 'ace-window)
+(global-set-key
+(kbd "M-o")
+#'ace-window))
 (require 'init-exec-path)
 (require 'init-minibuffer)
 (require 'init-company)
 (require 'init-org)
-
 (require 'init-lisp)
 (require 'init-md)
 (require 'init-haskell)
 (require 'init-nix)
-
 (require 'init-edit)
 (require 'init-lsp)
 (require 'init-git)
@@ -84,43 +72,42 @@
 (require 'init-ibuffer)
 (require 'init-session)
 (require 'init-undo-tree)
-
 (require 'init-zh-utils)
 
-
-(when (require 'eaf nil t)
-  (require 'eaf-airshare)
-  (require 'eaf-browser)
-  (require 'eaf-camera)
-  (require 'eaf-demo)
-  (require 'eaf-file-browser)
-  (require 'eaf-file-manager)
-  (require 'eaf-file-sender)
-  (require 'eaf-image-viewer)
-  (require 'eaf-jupyter)
-  (require 'eaf-markdown-previewer)
-  ;; (require 'eaf-mermaid)
-  (require 'eaf-mindmap)
-  (require 'eaf-music-player)
-  (require 'eaf-org-previewer)
-  (require 'eaf-pdf-viewer)
-  (require 'eaf-system-monitor)
-  (require 'eaf-terminal)
-  (require 'eaf-video-player)
-  (require 'eaf-vue-demo)
-  (require 'eaf-netease-cloud-music)
-  (require 'eaf-rss-reader))
-
+(when
+(require 'eaf nil t)
+(require 'eaf-airshare)
+(require 'eaf-browser)
+(require 'eaf-camera)
+(require 'eaf-demo)
+(require 'eaf-file-browser)
+(require 'eaf-file-manager)
+(require 'eaf-file-sender)
+(require 'eaf-image-viewer)
+(require 'eaf-jupyter)
+(require 'eaf-markdown-previewer)
+;; (require 'eaf-mermaid)
+(require 'eaf-mindmap)
+(require 'eaf-music-player)
+(require 'eaf-org-previewer)
+(require 'eaf-pdf-viewer)
+(require 'eaf-system-monitor)
+(require 'eaf-terminal)
+(require 'eaf-video-player)
+(require 'eaf-vue-demo)
+(require 'eaf-netease-cloud-music)
+(require 'eaf-rss-reader))
 (when system-darwin-p
-  (when (maybe-require-package 'ns-auto-titlebar)
-    (ns-auto-titlebar-mode)))
-
+(when
+(maybe-require-package 'ns-auto-titlebar)
+(ns-auto-titlebar-mode)))
 (add-hook 'after-init-hook
-	  (lambda ()
-	    (require 'server)
-	    (unless (server-running-p)
-	      (server-start))))
-
+(lambda
+()
+(require 'server)
+(unless
+(server-running-p)
+(server-start))))
 (provide 'init)
 ;; Local Variables:
 ;; coding: utf-8
--- emacs/lisp/init-zh-utils.el
+++ emacs/lisp/init-zh-utils.el
@@ -4,10 +4,16 @@
 
 ;;; Code:
 
-;; Pyim
-
-(when ixnij/enable-pyim-p
-  (when (maybe-require-package 'pyim)
+;; Input method for Chinese
+(if system-linux-p
+    (when
+	(maybe-require-package 'rime)
+      (require 'rime)
+      (setq default-input-method "rime")
+      (setq rime-librime-root "~/.emacs.d/librime/dist")
+      (setq rime-show-candidate 'posframe))
+  (when
+      (maybe-require-package 'pyim)
     (require 'pyim)
     (require-package 'pyim-basedict)
     (require 'pyim-basedict)
@@ -17,28 +23,13 @@
     (pyim-default-scheme 'quanpin)
     (pyim-isearch-mode 1)
     (diminish 'pyim-isearch-mode)
-    (if (posframe-workable-p)
+    (if
+	(posframe-workable-p)
 	(setq pyim-page-tooltip 'posframe)
       (setq pyim-page-tooltip 'popup))
     (setq pyim-page-length 5)
-    (when ixnij/pyim-use-rime-p
-      (require-package 'liberime)
-      (require 'liberime nil t)
-      (with-eval-after-load "liberime"
-	(liberime-try-select-schema "luna_pinyin_simp")
-	(setq pyim-default-scheme 'rime-quanpin)))))
-
+    ))
 
-
-;; Emacs-rime
-
-(when ixnij/enable-emacs-rime-p
-  (when (maybe-require-package 'rime)
-    (require 'rime)
-    (setq default-input-method "rime")
-    (setq rime-librime-root "~/.emacs.d/librime/dist")
-    (setq rime-emacs-module-header-root "/opt/homebrew/Cellar/emacs-plus@28/28.0.50/include")
-    (setq rime-show-candidate 'posframe)))
 
 ;; (when (maybe-require-package 'fanyi)
 ;;   (require 'fanyi)
@@ -90,7 +81,6 @@
 ;;
 ;;	 ;;(gts-kill-ring-render)
 ;;	 )))
-
 (provide 'init-zh-utils)
 ;; Local Variables:
 ;; coding: utf-8
