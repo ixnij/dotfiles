c5721f6577f24a3e7c0a4cf0a35e28123c8b364c
Thomas Wu <ixnij.wu@gmail.com>
2021-11-13 13:18:41 +0800

    [emacs, nix, zsh] update for my home

--- emacs/init.el
+++ emacs/init.el
@@ -11,39 +11,39 @@
       (version< emacs-version minver)
     (error "This Emacs is too old to use.-- You need to update to %s or higher" minver)))
 (when
-(version< emacs-version "27.1")
-(message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+    (version< emacs-version "27.1")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path
-(expand-file-name "lisp" user-emacs-directory))
+	     (expand-file-name "lisp" user-emacs-directory))
 
 (when
-(display-graphic-p)
-(tool-bar-mode -1)
-(scroll-bar-mode -1))
+    (display-graphic-p)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1))
 
 (defconst system-darwin-p
-(eq system-type 'darwin))
+  (eq system-type 'darwin))
 (defconst system-linux-p
-(eq system-type 'gnu/linux))
+  (eq system-type 'gnu/linux))
 
 (defvar ixnij/vertico-use-posframe-p nil)
 
 (require 'init-native-comp)
 (require 'init-mackeys)
-;; (require 'init-fonts)
+(require 'init-fonts)
 (let
-((normal-gc-cons-threshold
-(* 20 1024 1024))
-(init-gc-cons-threshold
-(* 128 1024 1024)))
-(setq gc-cons-threshold init-gc-cons-threshold)
-(add-hook 'emacs-startup-hook
-(lambda
-()
-(setq gc-cons-threshold normal-gc-cons-threshold))))
+    ((normal-gc-cons-threshold
+      (* 20 1024 1024))
+     (init-gc-cons-threshold
+      (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+	    (lambda
+	      ()
+	      (setq gc-cons-threshold normal-gc-cons-threshold))))
 (setq custom-file
-(expand-file-name "custom.el" user-emacs-directory))
+      (expand-file-name "custom.el" user-emacs-directory))
 (require 'init-site-lisp)
 (require 'init-elpa)
 (package-initialize)
@@ -52,10 +52,10 @@
 (require 'init-misc)
 (require 'init-evil)
 (when
-(maybe-require-package 'ace-window)
-(global-set-key
-(kbd "M-o")
-#'ace-window))
+    (maybe-require-package 'ace-window)
+  (global-set-key
+   (kbd "M-o")
+   #'ace-window))
 (require 'init-exec-path)
 (require 'init-minibuffer)
 (require 'init-company)
@@ -75,39 +75,39 @@
 (require 'init-zh-utils)
 
 (when
-(require 'eaf nil t)
-(require 'eaf-airshare)
-(require 'eaf-browser)
-(require 'eaf-camera)
-(require 'eaf-demo)
-(require 'eaf-file-browser)
-(require 'eaf-file-manager)
-(require 'eaf-file-sender)
-(require 'eaf-image-viewer)
-(require 'eaf-jupyter)
-(require 'eaf-markdown-previewer)
-;; (require 'eaf-mermaid)
-(require 'eaf-mindmap)
-(require 'eaf-music-player)
-(require 'eaf-org-previewer)
-(require 'eaf-pdf-viewer)
-(require 'eaf-system-monitor)
-(require 'eaf-terminal)
-(require 'eaf-video-player)
-(require 'eaf-vue-demo)
-(require 'eaf-netease-cloud-music)
-(require 'eaf-rss-reader))
+    (require 'eaf nil t)
+  (require 'eaf-airshare)
+  (require 'eaf-browser)
+  (require 'eaf-camera)
+  (require 'eaf-demo)
+  (require 'eaf-file-browser)
+  (require 'eaf-file-manager)
+  (require 'eaf-file-sender)
+  (require 'eaf-image-viewer)
+  (require 'eaf-jupyter)
+  (require 'eaf-markdown-previewer)
+  ;; (require 'eaf-mermaid)
+  (require 'eaf-mindmap)
+  (require 'eaf-music-player)
+  (require 'eaf-org-previewer)
+  (require 'eaf-pdf-viewer)
+  (require 'eaf-system-monitor)
+  (require 'eaf-terminal)
+  (require 'eaf-video-player)
+  (require 'eaf-vue-demo)
+  (require 'eaf-netease-cloud-music)
+  (require 'eaf-rss-reader))
 (when system-darwin-p
-(when
-(maybe-require-package 'ns-auto-titlebar)
-(ns-auto-titlebar-mode)))
+  (when
+      (maybe-require-package 'ns-auto-titlebar)
+    (ns-auto-titlebar-mode)))
 (add-hook 'after-init-hook
-(lambda
-()
-(require 'server)
-(unless
-(server-running-p)
-(server-start))))
+	  (lambda
+	    ()
+	    (require 'server)
+	    (unless
+		(server-running-p)
+	      (server-start))))
 (provide 'init)
 ;; Local Variables:
 ;; coding: utf-8
--- nix/home.nix
+++ nix/home.nix
@@ -1,43 +1,43 @@
 { config, pkgs, callPackage, ... }:
 
 {
-# Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
-# Home Manager needs a bit of information about you and the
-# paths it should manage.
-    home.username = "wu";
-    home.homeDirectory = "/Users/wu";
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "wu";
+  home.homeDirectory = "/Users/wu";
 
-# This value determines the Home Manager release that your
-# configuration is compatible with. This helps avoid breakage
-# when a new Home Manager release introduces backwards
-# incompatible changes.
-#
-# You can update Home Manager without changing this value. See
-# the Home Manager release notes for a list of state version
-# changes in each release.
-    home.stateVersion = "21.11";
-    home.packages = pkgs.callPackage ./packages.nix {};
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+  home.packages = pkgs.callPackage ./packages.nix {};
 
-#    programs.fish.enable = true;
-    programs.broot.enable = true;
-#    programs.broot.enableFishIntegration = true;
-    programs.broot.enableZshIntegration = true;
-    programs.skim.enable = true;
-#    programs.skim.enableFishIntegration = true;
-    programs.skim.enableZshIntegration = true;
-    programs.zoxide.enable = true;
-#    programs.zoxide.enableFishIntegration = true;
-    programs.zoxide.enableZshIntegration = true;
-    programs.nnn.enable = true;
+  #    programs.fish.enable = true;
+  programs.broot.enable = true;
+  #    programs.broot.enableFishIntegration = true;
+  programs.broot.enableZshIntegration = true;
+  programs.skim.enable = true;
+  #    programs.skim.enableFishIntegration = true;
+  programs.skim.enableZshIntegration = true;
+  programs.zoxide.enable = true;
+  #    programs.zoxide.enableFishIntegration = true;
+  programs.zoxide.enableZshIntegration = true;
+  programs.nnn.enable = true;
 
-    services.emacs.package = pkgs.emacsUnstable;
+  services.emacs.package = pkgs.emacsUnstable;
 
-    nixpkgs.overlays = [
-        (import (builtins.fetchTarball {
-                 url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-                 }))
-    ];
-
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = "https://github.com/nix-community/emacs-overlay/archive/a5de96c1781878c652bfe289caa75b521d20892f.tar.gz";
+      sha256 = "1ynz65h0ibykrz2w62ji5j8z2cgqwfxsd6q6pmgvbzb7wkm2w1kd";
+    }))
+  ];
 }
