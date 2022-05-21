2f10bd5da7e5df358e438a86844f9b5166c70859
Thomas Wu <ixnij.wu@gmail.com>
2022-05-21 11:17:49 +0800

    [emacs] remove hack for vertico

--- emacs/lisp/init-minibuffer.el
+++ emacs/lisp/init-minibuffer.el
@@ -23,43 +23,6 @@
   (vertico-mode)
   (vertico-reverse-mode 'toggle))
 
-;; @see https://github.com/minad/vertico/wiki
-;; Pre-select previous directory when entering parent directory from within find-file
-;; {
-
-;; Advise vertico-directory-up to save the directory being exited.
-
-(defvar previous-directory nil
-    "The directory that was just left. It is set when leaving a directory and
-    set back to nil once it is used in the parent directory.")
-
-(defun set-previous-directory ()
-  "Set the directory that was just exited from within find-file."
-  (when (> (minibuffer-prompt-end) (point))
-    (save-excursion
-      (goto-char (1- (point)))
-      (when (search-backward "/" (minibuffer-prompt-end) t)
-        ;; set parent directory
-        (setq previous-directory (buffer-substring (1+ (point)) (point-max)))
-        ;; set back to nil if not sorting by directories or what was deleted is not a directory
-        (when (not (string-suffix-p "/" previous-directory))
-          (setq previous-directory nil))
-        t))))
-
-(advice-add #'vertico-directory-up :before #'set-previous-directory)
-
-;; Advise vertico--update-candidates to select the previous directory.
-
-(define-advice vertico--update-candidates (:after (&rest _) choose-candidate)
-    "Pick the previous directory rather than the prompt after updating candidates."
-    (cond
-     (previous-directory ; select previous directory
-      (setq vertico--index (or (seq-position vertico--candidates previous-directory)
-                               vertico--index))
-      (setq previous-directory nil))))
-
-;; }
-
 ;; Optionally use the `orderless' completion style. See
 ;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
 ;; dispatcher. @see https://github.com/minad/consult/wiki
