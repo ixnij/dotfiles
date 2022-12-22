b98902402e80f56b04239ea40e844949dc4ab142
Thomas Wu <ixnij.wu@gmail.com>
2022-12-22 12:25:20 +0800

    [emacs] drop electric-pair-mode, use paredit instead

--- emacs/lisp/init-edit.el
+++ emacs/lisp/init-edit.el
@@ -60,18 +60,69 @@
   :hook (prog-mode . display-fill-column-indicator-mode))
 
 ;; Automatic parenthesis pairing
-(use-package elec-pair
-  :ensure nil
-  :hook ((emacs-lisp-mode . electric-pair-local-mode)
-	 (c-or-c++-mode . electric-pair-local-mode)
-	 (rust-mode . electric-pair-local-mode)
-	 (haskell-mode . electric-pair-local-mode))
-  :init
-  (setq
-   electric-pair-inhibit-predicate
-   'electric-pair-conservative-inhibit))
+;;(use-package elec-pair
+  ;;:ensure nil
+  ;;:hook ((emacs-lisp-mode . electric-pair-local-mode)
+	 ;;(c-or-c++-mode . electric-pair-local-mode)
+	 ;;(rust-mode . electric-pair-local-mode)
+	 ;;(haskell-mode . electric-pair-local-mode))
+  ;;:init
+  ;;(setq
+   ;;electric-pair-inhibit-predicate
+;;'electric-pair-conservative-inhibit))
+
+;; see https://www.emacswiki.org/emacs/ParEdit
+(use-package paredit
+  :ensure t
+  :config
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+  (use-package eldoc
+    :ensure nil
+    :config
+    (eldoc-add-command
+     'paredit-backward-delete
+     'paredit-close-round))
+  (defvar electrify-return-match
+    "[\]}\)\"]"
+    "If this regexp matches the text after the cursor, do an \"electric\"
+  return.")
+  (defun electrify-return-if-match (arg)
+    "If the text after the cursor matches `electrify-return-match' then
+  open and indent an empty line between the cursor and the text.  Move the
+  cursor to the new line."
+    (interactive "P")
+    (let ((case-fold-search nil))
+      (if (looking-at electrify-return-match)
+      (save-excursion (newline-and-indent)))
+      (newline arg)
+      (indent-according-to-mode)))
+  (defun electrify-return-hookfn ()
+    (local-set-key (kbd "RET") 'electrify-return-if-match))
+  (add-hook 'lisp-mode-hook #'electrify-return-hookfn)
+  (add-hook 'emacs-lisp-mode-hook #'electrify-return-hookfn)
+  (add-hook 'scheme-mode-hook #'electrify-return-hookfn)
+  (add-hook 'ielm-mode-hook #'electrify-return-hookfn)
+  (add-hook 'lisp-interaction-mode-hook #'electrify-return-hookfn)
+  (add-hook 'emacs-lisp-mode-hook
+	    (lambda ()
+	      (paredit-mode t)
+	      (turn-on-eldoc-mode)
+	      (eldoc-add-command
+	       'paredit-backward-delete
+	       'paredit-close-round)
+	      (local-set-key (kbd "RET") 'electrify-return-if-match)
+	      (eldoc-add-command 'electrify-return-if-match)
+	      (show-paren-mode t)))
+  )
 
 (provide 'init-edit)
+
 ;;; Local Variables:
 ;;; coding: utf-8
 ;;; End:
